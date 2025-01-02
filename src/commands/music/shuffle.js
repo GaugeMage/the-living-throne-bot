537286b3e138e8222f9a7a1b22d32f6fe30d2d03
GaugeMage <rkakkilaya@gmail.com>
2025-01-01 23:26:07 -0500

    TODO: Make shuffle reverse compatible

--- src/commands/music/shuffle.js
+++ src/commands/music/shuffle.js
@@ -1,4 +1,5 @@
 exports.run = async(player, message) => {
+    //TODO: Add flexibility to queue
     const queue = player.getQueue(message.guild);
     if(!queue || !queue.playing) return await message.reply("No music is being played");
     const success = queue.shuffle();
